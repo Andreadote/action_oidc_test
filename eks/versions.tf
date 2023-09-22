6d17bea66cdf602e50d56942d66b6ef3ce8f32f5
andreadote <andre.mj@yahoo.com>
2023-09-22 13:17:29 -0600

    fix: 22bug removed
    Contributing-to: https://kennedymakhanu1.atlassian.net/browse/<JIRA-TICKET# CLD-10>
    
    Problem
    Solution
    Note

---
+++ eks/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  #required_version = "~> 1.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.13.11"
+    }
+  }
+}
